8e186f2f258af2f6056e3e6560f4ac1ad5b8982c
aizriel1123 <aizriel1123@gmail.com>
2022-11-17 13:57:00 +0800

    Initial commit

---
+++ Deduyo_FirstPythonProgram.py
@@ -0,0 +1,5 @@
+name = input("Enter your Name: ")
+age = input("Enter your age: ")
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+print("The username is " + name + " and the age is " + age + "\nThe product is ", first * second)
